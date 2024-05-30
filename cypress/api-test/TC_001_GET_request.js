f566e62e939bf2af8e132da37d45fc6537eca270
Audrey1618 <audrey1618.uxi@gmail.com>
2024-05-30 20:29:07 +0700

    Stimulate failed test

--- cypress/api-test/TC_001_GET_request.js
+++ cypress/api-test/TC_001_GET_request.js
@@ -1,11 +1,11 @@
 describe('Test GET request', () => {
-    it('Should be able to send GET request and verify the response', () => {
-        cy.request({
+	it('Should be able to send GET request and verify the response', () => {
+		cy.request({
 			url: 'https://jsonplaceholder.typicode.com/posts',
 			method: 'GET',
 		}).then((response) => {
-            expect(response.status).to.eq(201)
-            expect(response.body.length).to.eq(100)
+			expect(response.status).to.eq(201);
+			expect(response.body.length).to.eq(200);
 		});
-    })
-})
+	});
+});
